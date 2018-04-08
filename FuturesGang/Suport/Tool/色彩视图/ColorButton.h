cd2dd234a10e902680a805017bab5ef76ffd7450
花满楼 <1563716978@qq.com>
2018-04-08 16:59:49 +0800

    1

--- FuturesGang/Suport/Tool/色彩视图/ColorButton.h
+++ FuturesGang/Suport/Tool/色彩视图/ColorButton.h
@@ -11,6 +11,8 @@
 @interface ColorButton : UIButton
 
 
-@property(nonatomic,strong) NSString * stateString; //1 红色  2 灰色  3 绿色 
+@property(nonatomic,strong) NSString * stateString; //1 红色  2 灰色  3 绿色
+
+@property(nonatomic)BOOL isnoBack;//1 无背景  0 有背景
 
 @end
--- FuturesGang/Suport/Tool/色彩视图/ColorButton.m
+++ FuturesGang/Suport/Tool/色彩视图/ColorButton.m
@@ -17,12 +17,42 @@
 {
     if (self = [super init]) {
         
-        [self makeUI];
+        //[self makeUI];
     }
     
     return self;
 }
 
+-(void)setmyisnoBack:(BOOL)isnoBack{
+    self.isnoBack  = isnoBack;
+    if (self.isnoBack) {
+        
+        [self makeNObackUI];
+    }else
+    {
+        [self makeUI];
+    }
+}
+
+
+-(void)makeNObackUI{
+    if ([self.stateString isEqualToString:@"1"] ) {
+        [self setTitleColor:APP_RED forState:UIControlStateNormal];
+    }else if ( [self.stateString isEqualToString:@"2"]){
+        
+        [self setTitleColor:APP_Gray forState:UIControlStateNormal];
+    }else if ( [self.stateString isEqualToString:@"2"]){
+        [self setTitleColor:APP_Green forState:UIControlStateNormal];
+    }
+
+    [self.titleLabel  sizeToFit];
+    self.titleLabel.adjustsFontSizeToFitWidth = YES;
+    
+    self.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+    
+//    [self.layer setMasksToBounds:YES];
+//    [self.layer setCornerRadius:3.0];
+}
 -(void)makeUI{
     
     if ([self.stateString isEqualToString:@"1"] ) {
