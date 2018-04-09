84a7427b429b8702adaeaa25fb833945e4d13801
花满楼 <1563716978@qq.com>
2018-04-09 15:21:50 +0800

    1

--- FuturesGang/我的/CenterChildVCs/银行卡/view/BankCardDTableViewCell.m
+++ FuturesGang/我的/CenterChildVCs/银行卡/view/BankCardDTableViewCell.m
@@ -22,6 +22,16 @@
 
 -(void)cellMakeUI{
     
+    
+    UILabel * cellLabel = [[UILabel alloc]init];
+    cellLabel.frame = CGRectMake(30*wb, 10*hb, SCREEN_WIDTH-60*wb, 70*hb);
+    cellLabel.numberOfLines = 2;
+    cellLabel.textColor =[UIColor whiteColor];
+    cellLabel.textAlignment = NSTextAlignmentCenter;
+    cellLabel.font = [UIFont systemFontOfSize:10];
+    cellLabel.text = @"持卡人必须与账户信息保持一致，请确保信息准确，以免影响您的买入与提现操作";
+    [self addSubview:cellLabel];
+    
     _tjrzButton = [self APPButtonmaker:@"提交认证" andtitleColor:[UIColor whiteColor]  backColor:APP_BLUE andfont:18.0];
     _tjrzButton.frame = CGRectMake((SCREEN_WIDTH-580*wb)/2, 180*hb, 580*wb, 80*hb);
     [_tjrzButton addTarget:self action:@selector(tjrzButtonClick) forControlEvents:UIControlEventTouchUpInside];
