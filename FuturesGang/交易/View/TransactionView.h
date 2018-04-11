b3e0ca3c57d9f94543abcc6b735b3bf4802a9cf6
花满楼 <1563716978@qq.com>
2018-04-11 17:31:21 +0800

    2

--- FuturesGang/交易/View/TransactionView.h
+++ FuturesGang/交易/View/TransactionView.h
@@ -9,8 +9,51 @@
 #import <UIKit/UIKit.h>
 #import "DropDownMenu.h"
 @interface TransactionView : UIView
+
+//下拉菜单部分
 @property(nonatomic,strong)DropDownMenu * menu;
 @property(nonatomic)CGFloat menuRowHeight;
 @property(nonatomic)CGFloat reH;
 @property(nonatomic)BOOL  isOpen;
+
+//手数
+@property(nonatomic,strong)UIButton * leftnumButton;
+@property(nonatomic,strong)UILabel  *  numLabel;
+@property(nonatomic,strong)UIButton * rightnumButton;
+
+//价格
+@property(nonatomic,strong)UIButton * leftpriceButton;
+@property(nonatomic,strong)UILabel  *  priceLabel;
+@property(nonatomic,strong)UIButton * rightpriceButton;
+
+//最新
+@property(nonatomic,strong)UILabel * freshLabel;
+//卖价
+@property(nonatomic,strong)UILabel * shellLabel;
+//买价
+@property(nonatomic,strong)UILabel * buyLabel;
+
+//红绿灰 三个按钮
+@property(nonatomic,strong)UIButton * redButton;
+@property(nonatomic,strong)UIButton * greenButton;
+@property(nonatomic,strong)UIButton * grayButton;
+
+//持仓
+
+@property(nonatomic,strong)UIButton * chicangButton;
+
+//可撤
+
+@property(nonatomic,strong)UIButton * kecheButton;
+
+//委托
+
+@property(nonatomic,strong)UIButton * weituoButton;
+
+//成交
+@property(nonatomic,strong)UIButton * chengjiaoButton;
+
+
+
+
 @end
--- FuturesGang/交易/View/TransactionView.m
+++ FuturesGang/交易/View/TransactionView.m
@@ -21,7 +21,147 @@
 
 
 -(void)makeUI{
+    //下拉
     [self DropDownMenuAdd];
+    
+    //手数
+    
+    [self numberView];
+    
+    //价格
+    [self priceView];
+}
+
+-(void)priceView{
+    
+    
+    
+    UILabel * jgLabel = [[UILabel alloc]init];
+    jgLabel.frame =CGRectMake(30*wb, 245*hb, 60*wb, 30*hb);
+    jgLabel.text = @"价格";
+    jgLabel.textColor = [UIColor whiteColor];
+    jgLabel.font = [UIFont systemFontOfSize:12];
+    [self addSubview:jgLabel];
+    
+    
+    
+    UIView  * bgview  =[[UIView alloc]init];
+    bgview.frame = CGRectMake(90*wb, 230*hb, 320*wb, 65*hb);
+    bgview.layer.masksToBounds = YES;
+    bgview.layer.cornerRadius = 4;
+    bgview.layer.borderWidth = 1;
+    bgview.layer.borderColor = APP_Gray.CGColor;
+    [self addSubview:bgview];
+    
+    //左侧按钮
+    
+    _leftpriceButton = [[UIButton alloc]init];
+    _leftpriceButton.frame = CGRectMake(0, 0, 65*hb, 65*hb);
+    [_leftpriceButton setBackgroundImage:[UIImage imageNamed:@"zuo"] forState:UIControlStateNormal];
+    [_leftpriceButton addTarget:self action:@selector(leftpriceButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [bgview addSubview:_leftpriceButton];
+    
+    _priceLabel = [[UILabel alloc]init];
+    _priceLabel.frame = CGRectMake(65*hb, 0, bgview.frame.size.width -130*hb, 65*hb);
+    _priceLabel.text = @"对手价";
+    _priceLabel.textColor = [UIColor whiteColor];
+    _priceLabel.textAlignment = NSTextAlignmentCenter;
+    [bgview addSubview:_priceLabel];
+    
+    //右侧按钮
+    
+    
+    _rightpriceButton = [[UIButton alloc]init];
+    _rightpriceButton.frame = CGRectMake(bgview.frame.size.width - 65*hb, 0, 65*hb, 65*hb);
+    [_rightpriceButton setBackgroundImage:[UIImage imageNamed:@"you"] forState:UIControlStateNormal];
+    [_rightpriceButton addTarget:self action:@selector(rightpriceButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [bgview addSubview:_rightpriceButton];
+}
+
+-(void)leftpriceButtonClick{
+   
+    NSInteger i ;
+    if ([_priceLabel.text isEqualToString: @"对手价"]) {
+        i = 0;
+    }else
+    {
+        i = [_priceLabel.text integerValue];
+    }
+    if ( i-1 == 0) {
+        _priceLabel.text = @"对手价";
+    }else{
+    _priceLabel.text = [NSString stringWithFormat:@"%ld",i-1];
+    }
+}
+-(void)rightpriceButtonClick{
+    
+    NSInteger i ;
+    if ([_priceLabel.text isEqualToString: @"对手价"]) {
+        i = 0;
+    }else
+    {
+        i = [_priceLabel.text integerValue];
+    }
+    if (i+1 == 0) {
+        _priceLabel.text = @"对手价";
+    }else{
+        _priceLabel.text = [NSString stringWithFormat:@"%ld",i+1];
+    }
+}
+
+-(void)numberView{
+    
+    UILabel * ssLabel = [[UILabel alloc]init];
+    ssLabel.frame =CGRectMake(30*wb, 145*hb, 60*wb, 30*hb);
+    ssLabel.text = @"手数";
+    ssLabel.textColor = [UIColor whiteColor];
+    ssLabel.font = [UIFont systemFontOfSize:12];
+    [self addSubview:ssLabel];
+    
+    
+    
+    UIView  * bgview  =[[UIView alloc]init];
+    bgview.frame = CGRectMake(90*wb, 130*hb, 320*wb, 65*hb);
+    bgview.layer.masksToBounds = YES;
+    bgview.layer.cornerRadius = 4;
+    bgview.layer.borderWidth = 1;
+    bgview.layer.borderColor = APP_Gray.CGColor;
+    [self addSubview:bgview];
+    
+    //左侧按钮
+    
+    _leftnumButton = [[UIButton alloc]init];
+    _leftnumButton.frame = CGRectMake(0, 0, 65*hb, 65*hb);
+    [_leftnumButton setBackgroundImage:[UIImage imageNamed:@"zuo"] forState:UIControlStateNormal];
+    [_leftnumButton addTarget:self action:@selector(leftnumButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [bgview addSubview:_leftnumButton];
+    
+    _numLabel = [[UILabel alloc]init];
+    _numLabel.frame = CGRectMake(65*hb, 0, bgview.frame.size.width -130*hb, 65*hb);
+    _numLabel.text = @"1";
+    _numLabel.textColor = [UIColor whiteColor];
+    _numLabel.textAlignment = NSTextAlignmentCenter;
+    [bgview addSubview:_numLabel];
+    
+    //右侧按钮
+    
+    
+    _rightnumButton = [[UIButton alloc]init];
+    _rightnumButton.frame = CGRectMake(bgview.frame.size.width - 65*hb, 0, 65*hb, 65*hb);
+    [_rightnumButton setBackgroundImage:[UIImage imageNamed:@"you"] forState:UIControlStateNormal];
+    [_rightnumButton addTarget:self action:@selector(rightnumButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [bgview addSubview:_rightnumButton];
+    
+    
+}
+-(void)rightnumButtonClick{
+    
+    NSInteger i = [_numLabel.text integerValue];
+    _numLabel.text = [NSString stringWithFormat:@"%ld",i+1];
+}
+-(void)leftnumButtonClick{
+    NSInteger i = [_numLabel.text integerValue];
+    _numLabel.text = [NSString stringWithFormat:@"%ld",i-1];
 }
 -(void)DropDownMenuAdd{
     _menuRowHeight = 65*hb;
