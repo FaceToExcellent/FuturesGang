69ed2e49f35354d790cb1ee73f55f200efcd6412
花满楼 <1563716978@qq.com>
2018-04-13 15:59:40 +0800

    1

--- FuturesGang/Suport/Tool/三种状态按钮/YButton.m
+++ FuturesGang/Suport/Tool/三种状态按钮/YButton.m
@@ -74,32 +74,6 @@
     
 }
 
-//
-//- (void)saveImage:(UIImage *)image {
-//    NSArray *paths =NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
-//
-//
-//    NSString *filePath = [[paths objectAtIndex:0]stringByAppendingPathComponent:
-//                          [NSString stringWithFormat:@"demo.png"]];  // 保存文件的名称
-//
-//    BOOL result =[UIImagePNGRepresentation(image)writeToFile:filePath   atomically:YES]; // 保存成功会返回YES
-//    if (result == YES) {
-//        NSLog(@"保存成功");
-//    }
-//
-//}
-//
-//- (void)getImage {
-//
-//    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
-//
-//    NSString *filePath = [[paths objectAtIndex:0]stringByAppendingPathComponent:
-//                          [NSString stringWithFormat:@"demo.png"]];
-//    // 保存文件的名称
-//    UIImage *img = [UIImage imageWithContentsOfFile:filePath];
-//    NSLog(@"=== %@", img);
-//
-//}
 
 
 
